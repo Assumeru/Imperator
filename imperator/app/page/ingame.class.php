b283edea31e23a6b1342236e6d3fcad9b751987a
Assumeru <malusluminis@hotmail.com>
2015-04-22 21:15:27 +0200

    more ingame gui + better chat gui

--- imperator/app/page/ingame.class.php
+++ imperator/app/page/ingame.class.php
@@ -18,11 +18,14 @@ class InGame extends DefaultPage {
 			'zoomin' => $language->translate('Zoom in'),
 			'zoomout' => $language->translate('Zoom out'),
 			'mapalt' => $language->translate('Map of %1$s', $this->game->getMap()->getName()),
-			'mapurl' => $this->getMapURL()
+			'mapurl' => $this->getMapURL(),
+			'interface' => $this->getInterface($user)
 		))->getData());
+		$this->addChatJavascript($this->game->getId());
 		$this->setTitle($this->game->getName());
 		$this->setMainClass('container-fluid');
 		$this->addCSS('game.css');
+		$this->addJavascript('map.js');
 		parent::render($user);
 	}
 
@@ -32,5 +35,19 @@ class InGame extends DefaultPage {
 
 	protected function getFooter(\imperator\User $user) {
 		return '';
+	}
+
+	private function getInterface(\imperator\User $user) {
+		$language = $user->getLanguage();
+		return Template::getInstance('game_interface')->replace(array(
+			'tabRegions' => $language->translate('Regions'),
+			'tabTerritories' => $language->translate('Territories'),
+			'tabChat' => $language->translate('Chat'),
+			'tabLog' => $language->translate('Combat Log'),
+			'tabPlayers' => $language->translate('Players'),
+			'tabCards' => $language->translate('Cards'),
+			'tabSettings' => $language->translate('Settings'),
+			'chat' => $this->getChatBox($user)
+		))->getData();
 	}
 }
